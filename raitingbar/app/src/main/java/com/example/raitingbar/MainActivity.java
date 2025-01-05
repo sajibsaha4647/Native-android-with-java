9488a3bc830fcdf7b3fe621a03d540d7a695fdda
sajibsaha4647 <sksaha4648@gmail.com>
2025-01-05 23:44:46 +0600

    updated

---
+++ raitingbar/app/src/main/java/com/example/raitingbar/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.raitingbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    RatingBar ratingBar ;
+    TextView textView ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ratingBar = (RatingBar) findViewById(R.id.ratingBarId);
+        textView = (TextView) findViewById(R.id.ratingBarResultId);
+        textView.setText("here is your value: "+ratingBar.getProgress());
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                textView.setText("here is your value: "+ratingBar.getProgress());
+            }
+        });
+
+    }
+}
